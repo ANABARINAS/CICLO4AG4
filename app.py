d0e6d5962c74ba5ac13cbb798b9f39d0bcf6d468
briiangmz <briiangmz@gmail.com>
2022-10-27 23:37:29 -0500

    segundo commit

--- app.py
+++ app.py
@@ -1,8 +1,21 @@
 from flask import Flask
+import json
+from flask import jsonify
+from flask import request
+from flask_cors import CORS
+from waitress import serve
+
+
 app=Flask(__name__)
 
 #primera ruta
 @app.route('/')
 def index():
     return "hola mundo"
+    
+@app.route('/usuaruio/<post:post_id>')
+def show_post(post_id):
+    return f'post {post_id}'
+    
+
     
