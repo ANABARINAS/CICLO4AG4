fdc9c85a1365bdf3b4d5f65fab7ef1a5459e0658
anaibarinas <anaibarinas@gmail.com>
2022-11-10 12:35:52 -0500

    mongodbCrud

--- mongoDB.py
+++ mongoDB.py
@@ -1,10 +1,13 @@
 from pymongo import MongoClient
 from bson.objectid import ObjectId
-client = MongoClient("mongodb+srv://Ciclo4Grupo5:Jd12201998@ciclo4grupo5.jmc0esl.mongodb.net/?retryWrites=true&w=majority")
-db = client["app-Ciclo4Grupo5"]
+"""client = MongoClient("mongosh "mongodb+srv://cluster0.sbdwoto.mongodb.net/databaseg4" --apiVersion 1 --username anaibarinas")
+db = client["app-registraduria"]
+"""
+client = MongoClient("mongodb+srv://anaibarinas:AnaIsa1227@cluster0.sbdwoto.mongodb.net/?retryWrites=true&w=majority")
+db = client["app-registraduria"]
 
 print(db.list_collection_names())
-
+"""
 #CRUD de Candidatos
 
 #CREATE
@@ -15,8 +18,9 @@ _candidato = {
 "Segundo Apellido":"Urrego",
 "Nombre de partido politico":"pacto historico"
 }
-
-CandidatosCollection = db.Candidatos
+"""
+CandidatosCollection = db.Candidatos 
+"""
 Candidatos_id = CandidatosCollection.insert_one(_candidato).inserted_id
 
 print(Candidatos_id)
@@ -27,11 +31,11 @@ for Candidatos in CandidatosCollection.find():
     print(Candidatos)
 
 #DELETE
-busqueda = {"_id": ObjectId("")}
+busqueda = {"_id": ObjectId("636d0a7ed75fb9ad19fac790")}
 CandidatosCollection.delete_one(busqueda)
-
+"""
 #UPDATE
-busquedaUpdate = {"_id": ObjectId("6353004cb6ddf919fb02f03f")}
+busquedaUpdate = {"_id": ObjectId("636d0b5deede7a44f4173138")}
 update2 = {"$set": {"Nombre": "juan",
 "Segundo Nombre": "Manuel",
 "Primer Apellido": "Santos",
